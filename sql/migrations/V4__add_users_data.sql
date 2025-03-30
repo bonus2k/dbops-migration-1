eefa6e9ebb0e194655b91e28a3dbb2f80cbaf37b
Spartak Katvickiy <bonus2k@yande.ru>
2025-03-30 14:54:02 +0300

    Refactor post inserts to use SELECT for owner_id retrieval

--- sql/migrations/V4__add_users_data.sql
+++ sql/migrations/V4__add_users_data.sql
@@ -2,6 +2,7 @@ INSERT INTO users (name) VALUES
 ('user1'),
 ('user2');
 
-INSERT INTO posts (text, owner_id) VALUES
-('text test1', SELECT id FROM users WHERE name = 'user1'),
-('text test2', SELECT id FROM users WHERE name = 'user2'),
+INSERT INTO posts (text, owner_id)
+SELECT 'text test1', id FROM users WHERE name = 'user1'
+UNION ALL
+SELECT 'text test2', id FROM users WHERE name = 'user2';
