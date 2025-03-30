0565b092eed3bbaeb49e66928c3d53c47d1796b0
Spartak Katvickiy <bonus2k@yande.ru>
2025-03-30 14:48:58 +0300

    Fix syntax error in SQL migration script

--- sql/migrations/V4__add_users_data.sql
+++ sql/migrations/V4__add_users_data.sql
@@ -2,6 +2,6 @@ INSERT INTO users (name) VALUES
 ('user1'),
 ('user2');
 
-INSERT INTRO posts (text, owner_id) VALUES
+INSERT INTO posts (text, owner_id) VALUES
 ('text test1', SELECT id FROM users WHERE name = 'user1'),
 ('text test2', SELECT id FROM users WHERE name = 'user2'),
